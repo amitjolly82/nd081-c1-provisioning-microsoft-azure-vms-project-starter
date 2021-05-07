e871b30980a8f97a94b07c54aef7cdf37fb2c1f3
amitjolly82 <75559575+amitjolly82@users.noreply.github.com>
2021-05-06 22:22:58 -0700

    Update config.py
    updated ODBC cred

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'ndcmssqlserver.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ND-SQL-DB-CMS-PRJ'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ndsqladmin'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Jethalal123@'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Hetal123@'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
